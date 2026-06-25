d4ed74f90a11770b91b040ee11d906359c42d209
Tahreem <tahreemnoor86@gmail.com>
2026-06-26 02:55:00 +0500

    created routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetail from './pages/MovieDetail'
+import SeatLayout from './pages/SeatLayout'
+import MyBookings from './pages/MyBookings'
+import Favourite from './pages/Favourite'
+import {Toaster} from 'react-hot-toast'
+import Footer from './components/Footer'
+
 
 const App = () => {
+
+  {/* to hide navbar from admin site */}
+  const isAdminRoute=useLocation().pathname.startsWith('/admin')
+
   return (
-    <div>
-      
-    </div>
+    <>
+    <Toaster/>
+      {!isAdminRoute && <Navbar/>}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/movies' element={<Movies/>}/>
+        <Route path='/movies/:id' element={<MovieDetail/>}/>
+        <Route path='/movies/:id/:date' element={<SeatLayout/>}/>
+        <Route path='/my-bookings' element={<MyBookings/>}/>
+        <Route path='/favourite' element={<Favourite/>}/>
+      </Routes>
+      {!isAdminRoute && <Footer/>}
+    </>
   )
 }
 
