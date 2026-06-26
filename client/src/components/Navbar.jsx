edbd631a50165eb502f17787daa89c4291495754
Tahreem <tahreemnoor86@gmail.com>
2026-06-26 16:58:30 +0500

    added user authentication using clerk

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
-import { MenuIcon, SearchIcon, XIcon } from 'lucide-react'
+import { MenuIcon, SearchIcon, TicketPlus, XIcon } from 'lucide-react'
+import { useClerk, UserButton, useUser } from '@clerk/react'
 
 const Navbar = () => {
 
     const [isOpen, setIsOpen] = useState(false)
+    const { user } = useUser()
+    const { openSignIn } = useClerk()
 
+    const navigate = useNavigate()
 
     return (
         <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
@@ -22,23 +26,36 @@ const Navbar = () => {
             duration-300 ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>
 
                 <XIcon
-                className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' 
-                onClick={()=>setIsOpen(!isOpen)}
+                    className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer'
+                    onClick={() => setIsOpen(!isOpen)}
                 />
 
-                <Link onClick={()=>{scrollTo(0,0), setIsOpen(!isOpen)}}to='/'>Home</Link>
-                <Link onClick={()=>{scrollTo(0,0), setIsOpen(!isOpen)}}to='/movies'>Movies</Link>
-                <Link onClick={()=>{scrollTo(0,0), setIsOpen(!isOpen)}}to='/'>Theaters</Link>
-                <Link onClick={()=>{scrollTo(0,0), setIsOpen(!isOpen)}}to='/'>Releases</Link>
-                <Link onClick={()=>{scrollTo(0,0), setIsOpen(!isOpen)}}to='/favourite'>Favourites</Link>
+                <Link onClick={() => { scrollTo(0, 0), setIsOpen(!isOpen) }} to='/'>Home</Link>
+                <Link onClick={() => { scrollTo(0, 0), setIsOpen(!isOpen) }} to='/movies'>Movies</Link>
+                <Link onClick={() => { scrollTo(0, 0), setIsOpen(!isOpen) }} to='/'>Theaters</Link>
+                <Link onClick={() => { scrollTo(0, 0), setIsOpen(!isOpen) }} to='/'>Releases</Link>
+                <Link onClick={() => { scrollTo(0, 0), setIsOpen(!isOpen) }} to='/favourite'>Favourites</Link>
             </div>
 
             <div className='flex items-center gap-8'>
-                <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer'/>
-                <button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>login</button>
+                <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer' />
+
+                {/* if user is not logged in it will show button else profile of user */}
+                {
+                    !user ? (
+                        <button onClick={openSignIn} className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>login</button>
+                    ) : (
+                        <UserButton>
+                            <UserButton.MenuItems>
+                                <UserButton.Action label='My Bookings' labelIcon={<TicketPlus width={15} />} onClick={() => navigate('/my-bookings')} />
+                            </UserButton.MenuItems>
+                        </UserButton>
+                    )
+                }
+
             </div>
 
-            <MenuIcon className='max-md:ml-4 md:hidden w-8 h-8 cursor-pointer' onClick={()=>setIsOpen(!isOpen)}/>
+            <MenuIcon className='max-md:ml-4 md:hidden w-8 h-8 cursor-pointer' onClick={() => setIsOpen(!isOpen)} />
         </div>
     )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/react'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ClerkProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>,
 )
