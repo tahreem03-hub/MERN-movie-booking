fede67f1abf40287b865c261ecade48a2e355949
Tahreem <tahreemnoor86@gmail.com>
2026-07-02 23:51:33 +0500

    first route

--- server/controller/showController.js
+++ server/controller/showController.js
@@ -1,17 +1,20 @@
 // this function will fetch movie data from TMDB and send it in response
 
-export const getNowPlaywingMovies= async(req, res)=>{
+import axios from "axios";
+
+export const getNowPlayingMovies= async(req, res)=>{
     try {
         // we got this api end point from TMBD when ever we wil hit this api 
         // end point we willl get now playing movie list
 
         // we will get response from this api call whcih we will store in data object
-
         const {data} = await axios.get('https://api.themoviedb.org/3/movie/now_playing',{
-            header: {Authorization: `Bearer ${process.env.TMDB_API_KEY}`}
+            headers: {Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+             accept: "application/json"
+            }
         })
 
-        const movies = data.result;
+        const movies = data.results;
         res.json({success:true, movies:movies})
         
     } catch (error) {
--- server/routes/showRoutes.js
+++ server/routes/showRoutes.js
@@ -1,11 +1,10 @@
 import express from 'express'
-import { getNowPlaywingMovies } from '../controller/showController';
+import { getNowPlayingMovies } from '../controller/showController.js';
 
 // create router
 const showRouter = express.Router();
 
 // create endpoint in this router
-
-showRouter('/now-playing', getNowPlaywingMovies)
+showRouter.get('/now-playing', getNowPlayingMovies)
 
 export default showRouter;
