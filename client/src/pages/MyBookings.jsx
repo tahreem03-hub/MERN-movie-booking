3aa1cc62c982de25c7c19b52a2d9c1cf250b8487
Tahreem <tahreemnoor86@gmail.com>
2026-07-07 12:23:45 +0500

    My Bookings: Connecting to Real Database

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -4,19 +4,36 @@ import Loading from '../components/Loading'
 import BlurCircle from '../components/BlurCircle'
 import TimeForamt from '../lib/TimeForamt'
 import dateFormat from '../lib/dateFormat'
+import { useAppContext } from '../../context/AppContext'
 
 const MyBookings = () => {
+
+  const { user, getToken, axios, image_base_url } = useAppContext()
+
 
   const currency = import.meta.env.VITE_CURRENCY
   const [isLoading, setIsLoading] = useState(true)
   const [bookings, setBookings] = useState([])
 
-  const getMyBookings = () => {
-    setBookings(dummyBookingData)
-    setIsLoading(false)
+  const getMyBookings = async() => {
+    try {
+      const { data } = await axios.get('/api/user/my-bookings', {
+        headers: { Authorization: `Bearer ${await getToken()}` }
+      })
+      if (data.success) {
+        setBookings(data.bookings)
+      }
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setIsLoading(false)
+    }
   }
 
-  useEffect(() => getMyBookings(), [])
+  useEffect(() => {
+    if (user) {
+    getMyBookings()}
+  }, [user])
   return !isLoading ? (
     <div className='realtive px-6 md:px-16 lg:px-40 pt-30 md:pt-40 min-h-[80vh]'>
       <BlurCircle top='100px' left='100px' />
@@ -29,7 +46,7 @@ const MyBookings = () => {
         bg-primary/8 border border-primary/20 rounded-lg mt-4 p-2 max-w-3xl'
           key={index}>
           <div className='flex flex-col md:flex-row'>
-            <img src={item.show.movie.poster_path} alt=""
+            <img src={image_base_url + item.show.movie.poster_path} alt=""
               className='md:max-w-45 aspect-video h-auto object-cover object-bottom rounded'
             />
             <div className='flex flex-col p-4'>
@@ -53,7 +70,7 @@ const MyBookings = () => {
               <p><span className='text-gray-400'>Total Tickets:</span>{item.bookedSeats.length}</p>
               <p><span className='text-gray-400'>Seat Number:</span>{item.bookedSeats.join(', ')}</p>
 
-              
+
             </div>
           </div>
         </div>
